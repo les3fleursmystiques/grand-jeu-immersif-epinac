ce4e1ad73411bbde975ffb820524575272d6679a
les3fleursmystiques <writer.jd3f@gmail.com>
2025-02-13 14:31:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 console.log("✅ script.js est bien chargé !");
 
 window.testTelegram = function() {
-    alert("🚀 Le bouton fonctionne !");
-    console.log("✅ testTelegram() a été appelée !");
-};
-
-window.redirectToPayPal = function(event) {
-    event.preventDefault();
-    alert("🚀 Redirection vers PayPal !");
-    console.log("✅ redirectToPayPal() a été appelée !");
+    alert("🚀 testTelegram() fonctionne !");
 };
