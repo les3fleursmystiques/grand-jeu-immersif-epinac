8acfb8f2e1124e83ee98216c95d4d57558758c4e
les3fleursmystiques <writer.jd3f@gmail.com>
2025-02-13 10:58:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log("✅ Grand Jeu Immersif à Épinac chargé !");
 alert("✅ Le script est bien chargé !");
+
+function testTelegram() {
+    alert("🚀 Le bouton fonctionne !");
+}
+
+function redirectToPayPal(event) {
+    event.preventDefault();
+    alert("🚀 Redirection vers PayPal !");
+}
