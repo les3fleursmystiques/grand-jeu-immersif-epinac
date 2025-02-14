0660eee9d59808f314294f8343bf39e7a0977a1f
les3fleursmystiques <writer.jd3f@gmail.com>
2025-02-14 13:41:08 +0100

    Add files via upload

--- env.js
+++ env.js
@@ -1,4 +1,1 @@
-export default {
-    VITE_TELEGRAM_BOT_TOKEN: process.env.VITE_TELEGRAM_BOT_TOKEN || "",
-    VITE_TELEGRAM_CHAT_ID: process.env.VITE_TELEGRAM_CHAT_ID || ""
-};
+export default window.env || {};
