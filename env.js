a452c88f01f0b8187a29c71fe6d5d93bbc68fe4c
les3fleursmystiques <writer.jd3f@gmail.com>
2025-02-14 15:17:13 +0100

    Add files via upload

--- env.js
+++ env.js
@@ -1,1 +1,6 @@
-export default window.env || {};
+window.env = {
+    VITE_TELEGRAM_BOT_TOKEN: "TON_TOKEN_ICI",
+    VITE_TELEGRAM_CHAT_ID: "TON_CHAT_ID_ICI"
+};
+
+console.log("🟢 Variables Netlify stockées dans window.env :", window.env);
