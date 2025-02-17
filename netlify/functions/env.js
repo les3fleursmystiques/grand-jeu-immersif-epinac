e4095e0be9ce3450e2eca768c0689da4f86686b9
les3fleursmystiques <writer.jd3f@gmail.com>
2025-02-17 15:58:27 +0100

    Add files via upload

--- netlify/functions/env.js
+++ netlify/functions/env.js
@@ -1,5 +1,11 @@
 // Fonction serverless pour exposer les variables d’environnement sur Netlify
 exports.handler = async function () {
+    console.log("🔍 Vérification des variables d’environnement Netlify :", {
+        VITE_TELEGRAM_BOT_TOKEN: process.env.VITE_TELEGRAM_BOT_TOKEN,
+        VITE_TELEGRAM_CHAT_ID: process.env.VITE_TELEGRAM_CHAT_ID,
+        ABSTRACT_API_KEY: process.env.ABSTRACT_API_KEY
+    });
+
     return {
         statusCode: 200,
         headers: { "Content-Type": "application/json" }, // Ajout du header JSON
